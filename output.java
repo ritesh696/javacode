cd1d64ae7b9987b9fbd3f3a3c30b49b7942c6a92
Ritesh Patel <41837638+ritesh696@users.noreply.github.com>
2025-01-05 22:14:00 +0530

    Update output.java

--- output.java
+++ output.java
@@ -3,7 +3,6 @@ class Output {
     	
         System.out.println("1. println ");
         System.out.println("2. println ");
-    	
         System.out.print("1. print ");
         System.out.print("2. print");
     }
