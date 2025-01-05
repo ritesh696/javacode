b552a542639e15acd85a12502bc648e677e9cf03
Ritesh Patel <41837638+ritesh696@users.noreply.github.com>
2025-01-05 21:55:06 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -4,5 +4,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
          System.out.println("This is a first jenkins job"); 
+         System.out.println("This is a first jenkins job Demo 2"); 
     }
 }
